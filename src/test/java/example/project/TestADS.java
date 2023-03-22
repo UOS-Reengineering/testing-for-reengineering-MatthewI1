36fcc5db26f820f5450472ebc735009ef0a7d60d
Matthew Ingram <mingram1@sheffield.ac.uk>
2023-03-22 11:43:06 +0000

    fixing first tests

--- src/test/java/example/project/TestADS.java
+++ src/test/java/example/project/TestADS.java
@@ -7,6 +7,8 @@ import static org.mockito.Mockito.*;
 import example.project.domain.Scenario;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,7 +16,13 @@ public class TestADS {
 
     @Test
     public void testPrintPath() {
-        fail();
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+
+        ADS ads = new ADS("dummy");
+        ads.printPath();
+        assertEquals(bos.toString().trim(), ads.ADSPath);
         // delete the above line and implement this properly to test if ads.printPath() prints the ADSPath.
         // hint: see testHelloWorld() in our previous lab session.
     }
